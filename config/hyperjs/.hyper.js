9a76766e1404c7225b22f2241cc904e01ca9ac71
Gabriel <Gabriel Pedrosa>
2018-06-09 19:27:29 -0300

    Fix do caminho para pasta com cores do vim; separação das etapas de instalação

--- config/hyperjs/.hyper.js
+++ config/hyperjs/.hyper.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 13,
+    fontSize: 14,
 
     // font family with optional fallbacks
     fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
@@ -91,7 +91,11 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyper-statusline',
+    'hyper-chesterish',
+    'hypercwd'
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
--- installDependencies.sh
+++ installDependencies.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-WORKDIR=$(pwd)
-
-aptinstall="sudo apt install -y"
-aptupdate="sudo apt update"
-aptrepo="sudo add-apt-repository"
 function debinstall {
   echo $1
   sudo dpkg -i $1
@@ -16,33 +11,40 @@ function rmlink {
   ln -s $1 $2 
 }
 
+function log {
+  printf "\n\n\n"
+  echo \[$1\]
+}
+
+aptinstall="sudo apt install -y"
+aptupdate="sudo apt update"
+aptrepo="sudo add-apt-repository"
+
 $aptupdate
 $aptinstall build-essential curl wget unzip locate
 
-# HYPER
+log "HYPER"
 wget -O hyperjs.deb https://releases.hyper.is/download/deb
 debinstall hyperjs.deb
 rm hyperjs.deb
-rmlink $WORKDIR/config/hyperjs/.hyper.js ~/.hyper.js
+rmlink $PWD/config/hyperjs/.hyper.js ~/.hyper.js
 
-# ALBERT
+log "ALBERT"
 $aptrepo ppa:nilarimogard/webupd8
 $aptupdate
 $aptinstall albert
-rmlink $WORKDIR/config/albert/albert.conf ~/.config/albert/albert.conf
+rmlink $PWD/config/albert/albert.conf ~/.config/albert/albert.conf
 
-# VIM
+log "VIM"
 $aptinstall neovim
 mkdir -p ~/.config/nvim
-rmlink $WORKDIR/config/vim/color ~/.config/nvim/color
-rmlink $WORKDIR/config/vim/.vimrc ~/.config/nvim/init.vim
+rmlink $PWD/config/vim/colors ~/.config/nvim/colors
+rmlink $PWD/config/vim/.vimrc ~/.config/nvim/init.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
 vim +BundleInstall +qall
 
-# ZSH
+log "ZSH"
 $aptinstall zsh git-core
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s $(which zsh)
-rmlink $WORKDIR/config/zsh/.zshrc ~/.zshrc
-
-
+rmlink $PWD/config/zsh/.zshrc ~/.zshrc
